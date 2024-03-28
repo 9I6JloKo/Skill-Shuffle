76afc08e84f92c989aebe0d1a7e59a8ec291f2a3
Max <90319998+9I6JloKo@users.noreply.github.com>
2024-03-28 13:43:06 +0200

    profile start

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Row } from "react-bootstrap";
 import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
+import Profile from '../pages/Profile';
 import Chat from '../pages/Chat';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
@@ -11,35 +13,39 @@ function Content() {
     const { authUser } = useAuth();
 
     return (
-        <Container className='wrapper'>
+        <Row className='wrapperMain'>
             <Routes>
                 {authUser ? (
                     <Route path="/messenger" element={<Chat />} />
                 ) : (
                     <Route path="/messenger" element={<Navigate to="/sign-in" />} />
                 )}
-
+                {authUser ? (
+                    <Route path="/my-profile" element={<Profile />} />
+                ) : (
+                    <Route path="/my-profile" element={<Navigate to="/sign-in" />} />
+                )}
                 <Route path="/sign-in"
                     element={
                         // If user is authenticated, redirect to the Chat page
-                        authUser ? <Navigate to="/messenger" /> : <SignIn />
+                        authUser ? <Navigate to="/my-profile" /> : <SignIn />
                     }
                 />
                 <Route path="/sign-up"
                     element={
                         // If user is authenticated, redirect to the Chat page
-                        authUser ? <Navigate to="/messenger" /> : <SignUp />
+                        authUser ? <Navigate to="/my-profile" /> : <SignUp />
                     }
                 />
 
                 <Route path="/"
                     element={
                         // If user is authenticated, redirect to the Chat page otherwise redirect to the sign-in page
-                        authUser ? <Navigate to="/messenger" /> : <Navigate to="/sign-in" />
+                        authUser ? <Navigate to="/userProfile" /> : <Navigate to="/sign-in" />
                     }
                 />
             </Routes>
-        </Container>
+        </Row>
     )
 }
 
